48b72e698d76abc2fd88178a91d8c6361946e115
Ashley Alfred <ashley@sagerecruiting.me>
2020-05-17 13:25:58 -0400

    Update and rename Bubble_Sort.c to Bubble_and_Selection_Sort.c

--- Bubble_and_Selection_Sort.c
+++ Bubble_and_Selection_Sort.c
@@ -3,19 +3,14 @@
 void print_arr(int a[], int len);
 void arr_swap(int array[], int pos1, int pos2);
 void bubble_sort(int array[], int length);
+void selection_sort(int array[], int length);
 
 int main(void)
 {
     int arr[] = { 6, 12, 35, 11, 42 };
-    bubble_sort(arr, 5);
+    selection_sort(arr, 5);
     print_arr(arr, 5);
-    
-    int second_arr[] = { 25, 32, 11, 39, 10 };
-    bubble_sort(second_arr, 5);
-    print_arr(second_arr, 5);
 }
-
-
 
 void print_arr(int a[], int len)
 {
@@ -49,4 +44,20 @@ void bubble_sort(int array[], int length)
         }
     }
     while (did_swap == 1);
+}
+
+void selection_sort(int array[], int length)
+{
+    for (int i = 0; i < length - 1; i++)
+    {   
+        int smallest_index = i;
+        for (int j = i + 1; j < length; j++ )
+        {
+            if (array[j] < array[smallest_index])
+            {
+                smallest_index = j;
+            }
+        }
+        arr_swap(array, i, smallest_index);
+    }
 }
